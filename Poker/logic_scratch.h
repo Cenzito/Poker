d85a3393e69f0be9f83590c881452e4c3071af95
arturocduro <118131836+arturocduro@users.noreply.github.com>
2023-11-20 13:59:11 +0100

    Add files via upload

---
+++ Poker/logic_scratch.h
@@ -0,0 +1,44 @@
+#ifndef POKER_SCRATCH_H
+#define POKER_SCRATCH_H
+
+
+class poker_scratch
+{
+public:
+    poker_scratch();
+};
+
+class PlayerHand {//input player hand and cards in middle
+public:
+    //
+    //
+    //
+    //function best hand
+        // if (royal flush) ...
+        // else if (straight flush)...
+
+        // else (high card)...
+
+private:
+    //
+    //variable hand
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // POKER_SCRATCH_H
