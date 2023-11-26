e2c9db95191ac76e706a3ce3dedd8ab3cb86668e
Luca-Andrei Fechete <luca.fechete1234@gmail.com>
2023-11-26 21:32:59 +0100

    Initial structure

---
+++ app/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "speech_to_text.h"
+
+int main() {
+    
+    print_hello_world();
+
+
+    return 0;
+}
---
+++ src/SpeechToText/speech_to_text.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "speech_to_text.h"
+
+
+void print_hello_world() {
+    std::cout << "Hello World!\n";
+
+}
