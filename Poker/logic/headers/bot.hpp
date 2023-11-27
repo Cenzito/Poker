aa88dacc15c30c7d11a87f6a04ee8ae8b3aa3204
Chanis <chanis.yp@gmail.com>
2023-11-27 14:16:25 +0100

    Update bot.hpp

--- Poker/logic/headers/bot.hpp
+++ Poker/logic/headers/bot.hpp
@@ -5,7 +5,7 @@ public :
     Bot(const std::string& name, int initialChips, int level) : PokerPlayer(name, initialChips) {
         this -> level = level;
     };
+    virtual void action(int minAmount, int type);
 protected:
     int level;
-    virtual void action(int minAmount, int type);
 };
