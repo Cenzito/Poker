afbb7eaa67efb34ebac9601a30e05a929fdd6a79
Ariel Flahaut <125026430+sachaela@users.noreply.github.com>
2023-11-29 16:17:13 +0100

    feature
    poker game with no betting

--- Poker/logic_scratch.cpp
+++ Poker/logic_scratch.cpp
@@ -1,27 +1,143 @@
-#include "poker_scratch.h"
-
-#ifndef POKER_PLAYER_HPP
-#define POKER_PLAYER_HPP
-
+#include "logic_scratch.hpp"
 #include <vector>
 #include <string>
+#include <iostream>
+#include <random>
+Player::Player(const std::string& name, int initialChips) : name(name), chips(initialChips) {}
 
-class Logic_ {
-public:
-    PokerPlayer(const std::string& name, int initialChips);
+int Player::getChips() const {
+    return chips;
+}
 
-    int getChips() const;
-    void placeBet(int amount);
-    void receiveCards(const std::vector<std::string>& cards);
-    void showHand() const;
-    void winChips(int amount);
-    // Add more functions as needed
+void Player::placeBet(int amount) {
+    if (amount <= chips) {
+        chips -= amount;
+        std::cout << name << " bets " << amount << " chips." << std::endl;
+    } else {
+        std::cout << name << " cannot bet that amount." << std::endl;
+    }
+}
 
-private:
-    std::string name;
-    int chips;
-    std::vector<std::string> hand; // Represents the player's cards
-    // Add more attributes as needed
-};
+void Player::receiveCards(const std::vector<Card>& cards) {
+    hand = cards;
+}
 
-#endif // POKER_PLAYER_HPP
+
+void Player::showHand() const {
+    std::cout << name << "'s hand: ";
+    for (const auto& card : hand) {
+        std::cout << card << " ";
+    }
+    std::cout << std::endl;
+}
+
+void Player::winChips(int amount) {
+    chips += amount;
+    std::cout << name << " wins " << amount << " chips." << std::endl;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const Card& card) {
+    os << card.toString();
+    return os;
+}
+
+
+Card::Card(const std::string& suit, int value) : suit(suit), value(value) {}
+
+std::string Card::getSuit() const {
+    return suit;
+}
+
+int Card::getValue() const {
+    return value;
+}
+
+std::string Card::toString() const {
+    return std::to_string(value) + suit;
+}
+
+
+Deck::Deck() : currentCardIndex(0) {
+    std::string suits[4] = {"H", "D", "C", "S"};
+    for (const std::string& suit : suits) {
+        for (int value = 1; value <= 13; ++value) {
+            cards.emplace_back(suit, value);
+        }
+    }
+    shuffleDeck();
+}
+
+
+
+void Deck::shuffleDeck() {
+    
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    
+    
+    std::mt19937 engine(seed);
+    std::shuffle(cards.begin(), cards.end(), engine);
+    currentCardIndex = 0;
+}
+
+
+Card Deck::dealCard() {
+    if (currentCardIndex < cards.size()) {
+        return cards[currentCardIndex++];
+    } else {
+        throw std::out_of_range("No more cards in the deck");
+    }
+}
+
+Game_::Game_(int numOfPlayers) {
+    for (int i = 0; i < numOfPlayers; ++i) {
+        players.emplace_back("Player " + std::to_string(i + 1), 1000); //1000 cest les chips
+    }
+}
+
+void Game_::startGame() {
+    deck.shuffleDeck(); 
+
+    // deal two random cards
+    for (Player& player : players) {
+        std::vector<Card> hand = {deck.dealCard(), deck.dealCard()};
+        player.receiveCards(hand);
+        std::cout << player.getName() << "'s hand: ";
+        player.showHand();
+    }
+
+    
+    std::cout << "Burn: " << deck.dealCard().toString() << std::endl;
+
+    // Flop
+    std::cout << "Flop: ";
+    for (int i = 0; i < 3; ++i) {
+        Card card = deck.dealCard();
+        communityCards.push_back(card);
+        std::cout << card.toString() << " ";
+    }
+    std::cout << std::endl;
+
+    // bet
+
+    // burn and card
+    std::cout << "Burn: " << deck.dealCard().toString() << std::endl;
+    std::cout << "Turn: ";
+    Card turnCard = deck.dealCard();
+    communityCards.push_back(turnCard);
+    std::cout << turnCard.toString() << std::endl;
+
+    // bet
+
+    // river
+    std::cout << "Burn: " << deck.dealCard().toString() << std::endl;
+    std::cout << "River: ";
+    Card riverCard = deck.dealCard();
+    communityCards.push_back(riverCard);
+    std::cout << riverCard.toString() << std::endl;
+
+    //betting
+}
+
+
+
