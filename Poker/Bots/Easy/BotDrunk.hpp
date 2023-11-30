26cbc30c21cd50619618403d0a8baaafa7626ee5
Chanis <chanis.yp@gmail.com>
2023-11-30 11:33:16 +0100

    Update BotDrunk.hpp

--- Poker/Bots/Easy/BotDrunk.hpp
+++ Poker/Bots/Easy/BotDrunk.hpp
@@ -2,8 +2,6 @@
 
 class BotDrunk : public Bot {
 public :
-    BotDrunk(const std::string& name, int initialChips) : Bot(name, initialChips, 1) {
-        this -> level = level;
-    };
+    BotDrunk(const std::string& name, int initialChips) : Bot(name, initialChips, 1) {}
     virtual void action(int minAmount, int type);
 };
