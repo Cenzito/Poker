3764577848ebcd4333b734f878400e5316d264fc
Lazea Darius <92463892+cryptolazea@users.noreply.github.com>
2023-12-04 10:34:46 +0100

    Commented some lines and changed the configuration
    I did some changes so that I can run the code on VS. If some other parts of the project do not work as a result, it might be because of commented lines here.

--- Poker/main.cpp
+++ Poker/main.cpp
@@ -1,18 +1,20 @@
 //#include "./logic/headers/PokerPlayer.hpp"
 
-#include "./Visuals/MainWindow/mainwindow.hpp"
+// #include "./Visuals/MainWindow/mainwindow.hpp"
 #include "./GameLogic/Game.hpp"
-#include <QApplication>
+// #include <QApplication>
 
 
 int main(int argc, char *argv[]) {
     Game pokerGame(4);
     pokerGame.startGame();
-
+    //std::cout << "Hello World!" << std::endl;
     
     //QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
     //return a.exec();
+
+    return 0;
 }
 //testing
