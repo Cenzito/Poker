6ff8e9c98861f8e78197ec400610ff07965e7759
Anatole-JDM <anatole.jacquin-de-margerie@polytechnique.edu>
2023-12-04 14:40:23 +0100

    Poker Table Img
    Added poker table to gamewindow

--- Poker/Visuals/MainWindow/gamewindow.cpp
+++ Poker/Visuals/MainWindow/gamewindow.cpp
@@ -1,11 +1,16 @@
 #include "gamewindow.h"
 #include "ui_gamewindow.h"
+#include "QPixmap"
 
 GameWindow::GameWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::GameWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("C:/Users/anato/OneDrive/Desktop/Bachelor Year 2/CSE201/Poker/poker_table.jpeg");
+    int w = ui->label_table->width();
+    int l = ui->label_table->height();
+    ui->label_table->setPixmap(pix.scaled(w,l, Qt::KeepAspectRatio));
 }
 
 GameWindow::~GameWindow()
