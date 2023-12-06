a1a16120965c8e9ea687ac8364f22b954ec2a5eb
Remix375 <remi.guillou@polytechnique.edu>
2023-12-06 11:24:41 +0100

    removed methods from table class
    I removed all the methods from the table and PokerInfo classes and instead put all the logic in the Game class.
    This makes it more efficient to send the table to the players and relies more on the Game class to treat everything related to the logic.
    Table and PlayerInfo now just store data in public variables.

--- Poker/GameLogic/table.cpp
+++ Poker/GameLogic/table.cpp
@@ -1,42 +1,12 @@
 #include "table.hpp"
 
 
-Table::Table(){this-> seats = 5;}
-Table::Table(int seats){this->seats = seats;};
+Table::Table(){this-> seats = 5;smallBlindPlayer=0;SBValue = 25;BBValue=50;}
+Table::Table(int seats){this->seats = seats;smallBlindPlayer=0;SBValue = 25;BBValue=50;};
 
 
 
-//to implement
-void Table::resetHand(){return;}
-//to implement
-void Table::addBet(){return;}
-//to implement
-void Table::winMoney(std::string name, int amount){return;}
 
-int Table::getFreeSeat() {
-    for (int i = 0; i<seats; i++) {
-        if (!(playerInfo.find(i) == playerInfo.end())) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-
-void Table::addCard(Card card) {communityCards.push_back(card);}
-
-
-//new person joins table
-void Table::JoinTable(PlayerInfo player) {
-    if (player_num >= seats)  {
-        return;
-    } else {
-        playerInfo.insert({getFreeSeat(), player});
-    }
-}
-
-//to implement
-void Table::leaveTable(PlayerInfo player) {return;}
 
 
 
