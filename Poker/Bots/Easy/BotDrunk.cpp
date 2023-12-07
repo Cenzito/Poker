3d687f57be8c62eabd02032bf86ef8634a3e907a
Chanis <chanis.yp@gmail.com>
2023-12-07 10:32:48 +0100

    Update Bot.hpp

--- Poker/Bots/Easy/BotDrunk.cpp
+++ Poker/Bots/Easy/BotDrunk.cpp
@@ -1,5 +1,5 @@
 #include "BotDrunk.hpp"
 
-std::pair<std::string, int> BotDrunk::action(const Table* table, int minAmount, int type) {
-    return std::make_pair("I'm drunkedn.", 0);
+void BotDrunk::action(int minAmount, int type) {
+    return;
 }
--- Poker/Bots/Easy/MonkeyBot.hpp
+++ Poker/Bots/Easy/MonkeyBot.hpp
@@ -4,7 +4,4 @@ class MonkeyBot : public Bot {
 public :
     MonkeyBot(const std::string& name, int initialChips) : Bot(name, initialChips, 100) {}
     void action(int minAmount, int type);
-
-
-
 };
