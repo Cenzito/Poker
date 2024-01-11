24d8ba628d8ba00f1597f5ab17c8dd368d08cb43
Chanis <chanis.yp@gmail.com>
2024-01-11 10:13:13 +0100

    Update BotDrunk.cpp

--- Poker/Bots/Easy/BotDrunk.cpp
+++ Poker/Bots/Easy/BotDrunk.cpp
@@ -1,5 +1,11 @@
 #include "BotDrunk.hpp"
 
 void BotDrunk::action(int minAmount, int type) {
+    /*
+    if(this -> playerInfo.bet <= minAmount)
+        this -> allIn();
+    else
+        this -> placeBet(minAmount);
+    */
     return;
 }
