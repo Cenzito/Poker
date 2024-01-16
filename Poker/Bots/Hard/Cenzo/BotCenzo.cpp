e76660dd840aee518ed4d8bbd08b9cdb10358745
Leal-Koeksal <leal.koksal@polytechnique.edu>
2024-01-16 20:14:22 +0100

    modificatioins on the action function

--- Poker/Bots/Hard/Cenzo/BotCenzo.cpp
+++ Poker/Bots/Hard/Cenzo/BotCenzo.cpp
@@ -1,12 +1,49 @@
 #include "BotCenzo.hpp"
+#include "../../../GameLogic/table.hpp"
+#include <cmath>
+#include <random>
+
 
 
 void BotCenzo::Action(){
-    //win = Winning_Probability(tableInfo, , )
+    float max;
+    for (int i = 0; i < tableInfo.playerInfo.size(); i++) {
+        max += tableInfo.playerInfo[i].stack_size;
+    }
+    max += tableInfo.pot;
+    max = max / tableInfo.pot;
+
     //calculate hand proba of win using  Monte Carlo Proba Simulator
+    float win;
     //check previous call
+    int last_bet = tableInfo.playerInfo[tableInfo.current_player - 1].bet;
+    float x; //the % of winning
+    float y = last_bet / tableInfo.pot;
+
+    float col;
+    float row;
+
+    //check what square we are in
+    if (y <= 2) {
+        col = floor(x/0.05);
+        row = floor(y/0.1);
+    }
+    else {
+        col = floor(x/0.05);
+        row = floor((0.1 + (y - 2)) / ((max - 2) / 0.1));
+    }
+
     //refer to matrix for probability of fold
-    //if fold return -1
+
+    if (foldfunction[row][col] == 1) {
+        //fold
+    }
+
+    else {
+        if (static_cast<float>(rand()) / RAND_MAX < foldfunction[row][col] ) {
+            //fold
+        }
+    }
+
     //else refer to matrix for probability of raise
-    //call or raise respectively
 }
--- Poker/Bots/Hard/Cenzo/BotCenzo.hpp
+++ Poker/Bots/Hard/Cenzo/BotCenzo.hpp
@@ -9,10 +9,13 @@ class BotCenzo : public Bot {
 public:
     BotCenzo(const std::string& name) : Bot(name,7) {};
     //matrix raisefunction;
-    //matrix foldfunction;
-    void Action();
-    //matrix adjustraise;
+    std::vector<std::vector<float>> raisefunction = std::vector<std::vector<float>>(20, std::vector<float>(20, 0.5));
 
+    //matrix foldfunction;
+    std::vector<std::vector<float>> foldfunction = std::vector<std::vector<float>>(20, std::vector<float>(20, 0));
+
+    void Action();
+    //matrix adjustraise; what?
 
 };
 
